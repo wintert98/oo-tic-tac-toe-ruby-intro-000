7b9577945bf634037791705db4770364feb872f7
Thomas Winter <wintert98@gmail.com>
2020-10-27 17:12:53 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,29 @@
+class TicTacToe
+  def initialize
+    @board = board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+  
+    WIN_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+      ]
+  
+  
+  def display_board
+    print " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    print "-----------"
+    print " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    print "-----------"
+    print " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  def input_to_index(input)
+    gets.chomp = input
+    @input = input.to_i!
+  end
+end
