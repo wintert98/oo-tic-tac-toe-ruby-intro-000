c28a4beb39e4635b71f1ccdb1566e58020bea9e4
Thomas Winter <wintert98@gmail.com>
2020-10-27 19:29:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,7 +89,7 @@ class TicTacToe
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
-     if position_1 == position_2  && position_2 == position_3 && position_taken?(board, win_index_1)
+     if position_1 == position_2  && position_2 == position_3 && position_taken?(@board, win_index_1)
     return win_combination   
   end
 end
