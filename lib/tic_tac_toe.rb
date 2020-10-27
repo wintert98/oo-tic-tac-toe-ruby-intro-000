32525ca66635a98ff151656894eff5b40d262f83
Thomas Winter <wintert98@gmail.com>
2020-10-27 17:57:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ class TicTacToe
     print "-----------"
     print " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  def input_to_index(@input)
+  def input_to_index(input)
     @input = gets.strip
     @index = @input.to_i - 1
   end
