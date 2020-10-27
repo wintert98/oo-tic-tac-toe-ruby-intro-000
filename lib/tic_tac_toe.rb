01147d3aa3c2ef2710d947625325e2ec37c7d07f
Thomas Winter <wintert98@gmail.com>
2020-10-27 19:17:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,7 +71,7 @@ class TicTacToe
     index = input_to_index(input)
     if valid_move?(index)
       move(index, current_player)
-      display_board(@board)
+      display_board
     else
       puts "Player the move is invalid, please make another move"
     end
