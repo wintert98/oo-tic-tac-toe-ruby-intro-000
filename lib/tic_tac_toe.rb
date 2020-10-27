090bd5081bd8bc775c84fccdf93b6809b49b70f2
Thomas Winter <wintert98@gmail.com>
2020-10-27 18:38:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end
   
   def position_taken?(index)
-    if index == " " || index == "" || index == nil
+    if @board[index] == " " || index == "" || index == nil
       return false 
     else
       return true
